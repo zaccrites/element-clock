bd755b8a1914ad0a4592937108a02d6c710c058f
Zac Crites <zaccrites@hotmail.com>
2016-09-10 01:57:29 -0500

    Fixed pins and shift ordering
    These got re-oredered with the PCB layout and I forgot
    to change them in the firmware...

--- src/io.h
+++ src/io.h
@@ -13,12 +13,12 @@
 /**
     IO pin configuration.
 */
-#define IO_PIN_SHIFT_DATA            (1 << PA5)
-#define IO_PIN_SHIFT_CLOCK           (1 << PA4)
+#define IO_PIN_SHIFT_DATA            (1 << PA0)
+#define IO_PIN_SHIFT_CLOCK           (1 << PA2)
 #define IO_PIN_SHIFT_LATCH           (1 << PA3)
-#define IO_PIN_SHIFT_CLEAR           (1 << PA2)
-#define IO_PIN_ELEMENT_MODE_SWITCH   (1 << PA1)
-#define IO_PIN_SPEED_BUTTON          (1 << PA0)
+#define IO_PIN_SHIFT_CLEAR           (1 << PA1)
+#define IO_PIN_ELEMENT_MODE_SWITCH   (1 << PA5)
+#define IO_PIN_SPEED_BUTTON          (1 << PA4)
 
 
 /**
